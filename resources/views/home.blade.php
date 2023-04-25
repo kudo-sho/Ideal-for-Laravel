7b742e28750b00cfe4bdf17fc7435e7b2a962041
kudo-sho <82325056+kudo-sho@users.noreply.github.com>
2023-04-26 00:17:28 +0900

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,12 @@
 <html lang="ja">
 <head>
 <meta charset='utf-8'>
-<title>ホーム画面</title>
+<title>Restaurante IDEALLE</title>
 </head>
 <body>
+    <a href="home.jsp">
+    <div class="toplink">IDEALLE</div>
+    </a>
 <p>こんにちは!</p>
 </body>
 </html>
