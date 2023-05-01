db13c1edd219e3edd239152cb5dba69f3c442906
kudo-sho <82325056+kudo-sho@users.noreply.github.com>
2023-05-01 18:52:57 +0900

    処理記述中

---
+++ app/Http/Controllers/AdminMaintenanceCtr.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+
+class AdminMaintenanceCtr extends Controller
+{
+    public function index(Request $request){
+        //adminリストを取得してリクエストにセットする
+        $a = new Admin();
+        $adm_list = $a->getAdminList();
+
+    }
+}
+
