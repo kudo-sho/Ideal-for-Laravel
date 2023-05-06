8ba31d35b169d533b0c789abdedf8b80ed1ddfa7
kudo-sho <82325056+kudo-sho@users.noreply.github.com>
2023-05-06 20:02:48 +0900

    userモデル作成完了

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,4 +69,62 @@ class User extends Authenticatable
         }
         return $user;
     }
+
+    //ユーザー情報取得処理
+    public function getUser($userId) {
+        //管理者情報問い合わせ
+        $results = DB::select(
+            "SELECT * FROM users
+            WHERE id = :id",
+            ['id' => $userId]
+        );
+        return $results;
+    }
+
+    //ユーザー新規登録処理
+    public function insert(User $user){
+        //ユーザー情報をusersテーブルにinsert
+        $query = DB::insert(
+            "INSERT INTO users(name,password,email)
+            VALUES(:name,:password,:email)",[
+                'name'=>$user->getName(),
+                'password'=>$user->getPassword(),
+                'email'=>$user->getEmail()
+                ]
+        );
+
+        //新規登録した顧客IDを取得する
+        $results = DB::select(
+            "SELECT last_insert_id() as userInfo"
+        );
+
+        //顧客IDをセットする
+        $user->setId($results[0]->userInfo);
+        return $user;
+    }
+
+    //ユーザー情報変更
+    public static function updateUser(User $user){
+        //ユーザー情報をusersテーブルにupdate
+        $query = DB::update(
+            "UPDATE users SET
+            name = :name,email = :email
+            WHERE id = :id",[
+                'id'=>$user->getId(),
+                'name'=>$user->getName(),
+                'email'=>$user->getEmail()
+                ]
+        );
+        //パスワードの更新は別のメソッドにて実装予定
+        return $user;
+    }
+
+    //ユーザー登録削除
+    public static function deleteUser(int $userId){
+        //引数のidに一致するレコードをusersテーブルから削除
+        $query = DB::delete(
+            "DELETE FROM users WHERE id = :id",
+            ['id'=>$userId]
+        );
+    }
 }
